6e055cd5c4dcae39c952f14063a2aba5ce524644
Terry Garcia <terry@terryg.org>
2024-06-17 08:54:14 -0500

    Removed mailer stuff

--- lib/ps99strat_web/router.ex
+++ lib/ps99strat_web/router.ex
@@ -25,7 +25,7 @@ defmodule Ps99stratWeb.Router do
   #   pipe_through :api
   # end
 
-  # Enable LiveDashboard and Swoosh mailbox preview in development
+  # Enable LiveDashboard preview in development
   if Application.compile_env(:ps99strat, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
@@ -38,7 +38,6 @@ defmodule Ps99stratWeb.Router do
       pipe_through :browser
 
       live_dashboard "/dashboard", metrics: Ps99stratWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
 end
